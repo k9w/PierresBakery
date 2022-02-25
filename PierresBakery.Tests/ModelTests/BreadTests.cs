2af4765d64e3e65479909444f500508e98b9db33
Kevin Williams <kevin@k9w.org>
2022-02-25 18:56:54 +0000

    Add using, namespace, and other starter code for Bread.cs and its Tests.

---
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bread.Models;
+
+namespace Bread.Tests
+{
+  [TestClass]
+  {
+    public void Dispose();
+    {
+	Bread.ClearAll();
+    }
+    
+  }
+}
---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bread.Models
+{
+  public class Bread
+  {
+    public string Description
+  }
+}
